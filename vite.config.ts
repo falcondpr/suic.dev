be2ddb3f36bbfeb0ae155b84a902d75f506018ca
Fernando Villalba <21fervillalbag@gmail.com>
2024-05-27 23:57:47 -0400

    feat: config to upload at npm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: ".", // Colocar la salida en la raíz
+    lib: {
+      entry: "./src/index.ts",
+      formats: ["es", "cjs"], // Generar tanto ESM como CommonJS
+      name: "suic.dev",
+      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`, // Generar index.mjs para ESM y index.js para CommonJS
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
